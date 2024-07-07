ba76fbc1995ae5ac96f46e42f02f716bfa4417d3
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2024-07-07 08:22:02 +0530

    Add files via upload

---
+++ FirstClass .java
@@ -0,0 +1,12 @@
+import java.util.*;
+class FirstClass {
+    // Addition of two numbers
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.print("Addition of two numbers: ");
+        System.out.println(sum);
+    }
+}
